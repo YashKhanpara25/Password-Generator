9cc847ec571826fdaf3dbd4f66c00aa7e3166212
YashKhanpara <yashkhanpara25@gmail.com>
2022-10-05 14:00:05 +0530

    added generate password button

--- src/App.js
+++ src/App.js
@@ -1,23 +1,53 @@
-import logo from './logo.svg';
+import * as React from 'react';
+import Button from '@mui/material/Button';
+import TextField from '@mui/material/TextField';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
 import './App.css';
+import { useState } from 'react';
+import {generator} from './generator';
+
 
 function App() {
+
+  const [password, generate] = useState("Click Generate!!");
+  const generatePassword = () => {
+    let password = generator();
+    generate(password);
+  }
+  
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <Container component="main" maxWidth="xs">
+        <Box
+          sx={{
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+          }}
         >
-          Learn React
-        </a>
-      </header>
+          
+          <Typography component="h1" variant="h5">
+            Password Generator
+          </Typography>
+          <Typography component="h1" variant='h5' sx={{border : "2px solid grey", borderRadius : "5%", padding : "4px" ,width:"80%"}}>
+            {password}
+          </Typography>
+          <Box noValidate sx={{ mt: 1 }}>
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              sx={{ mt: 3, mb: 2 }}
+              onClick={generatePassword}
+            >
+              Generate
+            </Button>
+          </Box>
+        </Box>
+      </Container>
     </div>
   );
 }
