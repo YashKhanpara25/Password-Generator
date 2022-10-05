246f2ff7550619c70127e241cfd46c30d46d814b
Parin Jasoliya <parinjasoliya@gmail.com>
2022-10-05 14:42:15 +0530

    Implement Password Logic
    - Add Random Funcition to Generate Radom Ascii Values

--- src/generator.js
+++ src/generator.js
@@ -1,3 +1,20 @@
+
+function random(low, high){
+    if(high - low < 0){
+        high = low;
+    }
+    let num = low + (Math.random() * (high+1 - low))
+
+    return Math.floor(num);
+}
+
 export function generator () {
-    return "RandomPassword";
+    let len = 15;
+
+    let pass_string = "";
+
+    for(let i=0;i<len;i++){
+        pass_string += String.fromCharCode(random(33,172))
+    }
+    return pass_string;
 }
